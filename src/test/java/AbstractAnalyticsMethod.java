36a6721c9a96e08c98d98ec9bcedb3e5f8a43cca
lechip <lechip@gmail.com>
2015-11-15 20:53:13 +0100

    Initial commit

---
+++ src/test/java/AbstractAnalyticsMethod.java
@@ -0,0 +1,15 @@
+/**
+ * Created by lechip on 15/11/15.
+ */
+public abstract class AbstractAnalyticsMethod {
+    Boolean isPredictive;
+    public abstract String sayHi();
+
+    public AbstractAnalyticsMethod(Boolean isPredictive) {
+        this.isPredictive = isPredictive;
+    }
+
+    public Boolean getPredictive() {
+        return isPredictive;
+    }
+}
