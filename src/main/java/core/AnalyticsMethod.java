b145f8f9bdc3b9954d0c153c94f76b40bdcf4eaa
lechip <lechip@gmail.com>
2015-11-16 15:09:17 +0100

    - Added main methods for the abstract class

--- src/main/java/core/AnalyticsMethod.java
+++ src/main/java/core/AnalyticsMethod.java
@@ -1,9 +1,17 @@
 package core;
 
+import OLAPDataSet.*;
+
 /**
  * Created by lechip on 15/11/15.
  */
 public abstract class AnalyticsMethod {
-    public abstract String sayHi();
-}
+    OLAPDataSet input;
+    OLAPDataSet output;
+    Boolean isPredictive;
 
+    public abstract OLAPDataSet execute();
+    public abstract OLAPColumnConfigurationData getInputPorts();
+    public abstract OLAPColumnConfigurationData getOutputPorts();
+    public abstract void initialize(OLAPDataSet data, OLAPPortConfiguration configuration);
+}
